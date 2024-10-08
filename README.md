5100bde58970d285f43a9a99cf07edb7a03f37a7
Musta-Kissa <mustahattu@tutamail.com>
2024-10-08 21:03:50 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ![Screenshot](./screenshots/bevy_cubes.png)
 ![Screenshot](./screenshots/2d_noise_terrain.png)
 ![Screenshot](./screenshots/2d_noise_stack_terrain.png)
+![Screenshot](./screenshots/culled_chunk_borders.png)
--- src/world.rs
+++ src/world.rs
@@ -39,18 +39,18 @@ impl VoxelWorld {
         if voxel_pos.x == 0 {
             if let Some(chunk) = self.get_chunk(chunk_pos - IVec3::X) {
                 if !chunk.data.get(31, voxel_pos.y, voxel_pos.z) {
-                    directions.push(Direction::South); 
+                    directions.push(Direction::South);
                 }
-            }else{
-                    directions.push(Direction::South); 
+            } else {
+                directions.push(Direction::South);
             }
         } else if voxel_pos.x == CHUNK_SIZE - 1 {
             if let Some(chunk) = self.get_chunk(chunk_pos + IVec3::X) {
                 if !chunk.data.get(0, voxel_pos.y, voxel_pos.z) {
                     directions.push(Direction::North);
                 }
-            }else{
-                    directions.push(Direction::North);
+            } else {
+                directions.push(Direction::North);
             }
         }
         if voxel_pos.x != 0 && !chunk_data.get(voxel_pos.x - 1, voxel_pos.y, voxel_pos.z) {
@@ -65,19 +65,19 @@ impl VoxelWorld {
         if voxel_pos.y == 0 {
             if let Some(chunk) = self.get_chunk(chunk_pos - IVec3::Y) {
                 if !chunk.data.get(voxel_pos.x, 31, voxel_pos.z) {
-                    directions.push(Direction::Down) 
+                    directions.push(Direction::Down)
                 }
-            }else{
-                    //directions.push(Direction::Down)//Remove world floor 
-                }
+            } else {
+                //directions.push(Direction::Down) //Remove world floor
+            }
         } else if voxel_pos.y == CHUNK_SIZE - 1 {
             if let Some(chunk) = self.get_chunk(chunk_pos + IVec3::Y) {
                 if !chunk.data.get(voxel_pos.x, 0, voxel_pos.z) {
-                    directions.push(Direction::Up) 
-                }
-            }else{
                     directions.push(Direction::Up)
                 }
+            } else {
+                directions.push(Direction::Up)
+            }
         }
         if voxel_pos.y != 0 && !chunk_data.get(voxel_pos.x, voxel_pos.y - 1, voxel_pos.z) {
             directions.push(Direction::Down)
@@ -91,19 +91,19 @@ impl VoxelWorld {
         if voxel_pos.z == 0 {
             if let Some(chunk) = self.get_chunk(chunk_pos - IVec3::Z) {
                 if !chunk.data.get(voxel_pos.x, voxel_pos.y, 31) {
-                    directions.push(Direction::East) 
-                }
-            }else{
                     directions.push(Direction::East)
                 }
+            } else {
+                directions.push(Direction::East)
+            }
         } else if voxel_pos.z == CHUNK_SIZE - 1 {
             if let Some(chunk) = self.get_chunk(chunk_pos + IVec3::Z) {
                 if !chunk.data.get(voxel_pos.x, voxel_pos.y, 0) {
-                    directions.push(Direction::West) 
+                    directions.push(Direction::West)
                 }
-            }else{
-                    directions.push(Direction::West) 
-                }
+            } else {
+                directions.push(Direction::West)
+            }
         }
         if voxel_pos.z != 0 && !chunk_data.get(voxel_pos.x, voxel_pos.y, voxel_pos.z - 1) {
             directions.push(Direction::East)
